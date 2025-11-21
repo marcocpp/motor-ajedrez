2a565b029e785c6664392641d3f6319e2b88e223
marcocpp <marcocp150206@gmail.com>
2025-11-21 21:46:27 +0100

    Movimiento del caballo implementado

--- src/Caballo.cpp
+++ src/Caballo.cpp
@@ -4,6 +4,26 @@ Caballo::Caballo(Color color, char icono) : Ficha(color, icono) {
 
 }
 
+
+// El caballo se puede mover en forma de L desde la coordenada de origen
 bool Caballo::movimientoValido(Coordenada origen, Coordenada destino, bool esCaptura) {
-    return false;
+    // Si se pasa de alto o de largo
+    if (abs(origen.fila - destino.fila) > 2 || abs(origen.col - destino.col) > 2) {
+        return false;
+    }
+    else {
+        // Se esta moviendo en diagonal
+        if (abs(origen.fila - destino.fila) == abs(origen.col - destino.col)) {
+            return false;
+        }
+        else {
+            // Se mueven en horizontal o vertical
+            if (abs(origen.fila - destino.fila) == 0 || abs(origen.col - destino.col) == 0) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
 }
