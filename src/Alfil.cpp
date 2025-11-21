a24c12668a761519d049e5b21c36a62b1f57c5e5
marcocpp <marcocp150206@gmail.com>
2025-11-21 21:53:34 +0100

    Movimiento Alfil implementado

--- src/Alfil.cpp
+++ src/Alfil.cpp
@@ -2,6 +2,13 @@
 
 Alfil::Alfil(Color color, char icono) : Ficha(color, icono) {}
 
+// El alfil se mueve en diagonal
 bool Alfil::movimientoValido(Coordenada origen, Coordenada destino, bool esCaptura) {
-    return false;
+    // Se esta moviendo en diagonal
+    if (abs(origen.fila - destino.fila) == abs(origen.col - destino.col)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
