1cd5d7af237e6967073e77f71b0b0d5e6a147809
marcocpp <marcocp150206@gmail.com>
2025-11-22 11:48:19 +0100

    Movimiento Reina

--- src/Reina.cpp
+++ src/Reina.cpp
@@ -2,6 +2,13 @@
 
 Reina::Reina(Color color, char icono) : Ficha(color, icono) {}
 
+// Su movimiento es como la suma del alfil y la torre
 bool Reina::movimientoValido(Coordenada origen, Coordenada destino, bool esCaptura) {
+    if (abs(origen.fila - destino.fila) == abs(origen.col - destino.col)) {
+        return true;
+    }
+    else if (abs(origen.fila - destino.fila) == 0 || abs(origen.col - destino.col) == 0) {
+        return true;
+    }
     return false;
 }
